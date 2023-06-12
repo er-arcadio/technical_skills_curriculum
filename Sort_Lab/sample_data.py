a332a20b45af6dcd0fb731fcf12ec4c0ad232079
Emmanuel Rodriguez <er.arcadio@gmail.com>
2023-06-12 13:37:37 -0400

    Sort Lab

---
+++ Sort_Lab/sample_data.py
@@ -0,0 +1,52 @@
+students = [
+    {
+        "name":"Tanya",
+        "age": 26,
+        "occupation": "Lawyer"
+    },
+    {
+        "name":"Bradley",
+        "age": 37,
+        "occupation": "Astrologer"
+    },
+    {
+        "name":"Zakir",
+        "age": 20,
+        "occupation": "Builder"
+    },
+    {
+        "name":"Velma",
+        "age": 41,
+        "occupation": "Teacher"
+    },
+    {
+        "name":"Reggie",
+        "age": 25,
+        "occupation": "Stockbroker"
+    },
+    {
+        "name":"Abubakar",
+        "age": 30,
+        "occupation": "Marketing Director"
+    },
+    {
+        "name":"Elijah",
+        "age": 48,
+        "occupation": "Nutritionist"
+    },
+    {
+        "name":"Cole",
+        "age": 31,
+        "occupation": "Dental Nurse"
+    },
+    {
+        "name":"Ellen",
+        "age": 40,
+        "occupation": "Entrepreneur"
+    },
+    {
+        "name":"Sonia",
+        "age": 36,
+        "occupation": "Musician"
+    }
+]
