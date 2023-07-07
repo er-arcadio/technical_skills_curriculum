59b26d17f52be6fe3e995bd8adf29e9b73283a3d
Emmanuel Rodriguez <er.arcadio@gmail.com>
2023-07-07 09:32:27 -0400

    Linked list additions

--- Linked_List_Lab/train_cars.py
+++ Linked_List_Lab/train_cars.py
@@ -39,4 +39,16 @@ new_train = {
 
 # -- Spicy --
 
-# 
+# Write a function `reverse(train_cars, first_train)` that makes the last train car first and the first train car last.
+# The function should return the new first_train
+
+# Write a function sort_trains(train_cars, first_train). Use your favorite sorting algorithm to sort the trains by weight.
+# The heaviest should be at the front and lightest at the back.
+
+# [Super Spicy] Convert the linked list using object oriented programming.
+# - Create a new file `Link.py` that defines 2 classes: "Train" and "Car"
+# - Train should have the `first_train` variable and a methods to append a new train
+# - Car should have all the attributes in the dictionary above -- include "next_train" and "id"
+
+# [Super Spicy] Add and adjust `print_trains()` and the rest of the functions from the Medium challenges in your "Train" class
+# the print for each train should display the color, material, and weight.
